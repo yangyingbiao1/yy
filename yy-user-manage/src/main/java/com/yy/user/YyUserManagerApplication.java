d8ab9927ae2843f85866d3798a42ef374c1bc054
yangyingbiao1 <yyhr2017@outlook.com>
2020-01-08 22:10:35 +0800

    第二次

---
+++ yy-user-manage/src/main/java/com/yy/user/YyUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.yy.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YyUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YyUserManagerApplication.class, args);
+    }
+
+}
